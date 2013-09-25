6d9955d50a24381478736e3b01b52324912cf5ba
Mike Tierney <mike@panpainter.com>
2013-09-25 09:11:31 -0700

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Refreshsea::Application.config.secret_key_base = '754ef7b5a08fd8863ac99d6d6250a21215b57c7d0e9726f60b5dc0aa21c865ad95cce288160f99bc7132a72de09ac7d26b6a8a34e118d51699c0a377decc658d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Refreshsea::Application.config.session_store :cookie_store, key: '_refreshsea_session'
