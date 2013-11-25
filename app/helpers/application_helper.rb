6a3a3625a4b3d6e02e93d50b8451c0e2fabbdbe4
Mike Tierney <mike@panpainter.com>
2013-11-24 18:27:47 -0800

    Add markdown support.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
+require 'redcarpet'
+
 module ApplicationHelper
+  def markdown content
+    markdown = Redcarpet::Markdown.new Redcarpet::Render::HTML, autolink: true, space_after_headers: true
+    raw markdown.render content
+  end
 end
