abf1591b6318c79f23a69131e5202207ec811774
Mike Tierney <mike@panpainter.com>
2014-05-19 08:03:26 -0700

    Remove basic auth protections.
    We're open-sourcing this project, so basic-auth now is ... silly.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  if Rails.env == 'staging' or Rails.env == 'production'
-    http_basic_authenticate_with name: ENV['user'], password: ENV['password']
-  end
 end
